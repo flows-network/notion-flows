3fb66992bca9cf5387f9bfcf34f064d2a0edfe50
DarumaDocker <DarumaDocker@gmail.com>
2023-08-14 16:09:08 +0800

    feth_one will return error if there is no rows exist

--- integration/src/route/connected.rs
+++ integration/src/route/connected.rs
@@ -13,21 +13,23 @@ pub async fn connected(
     let mut results = Vec::new();
 
     let sql = "SELECT workspace_id, workspace_name FROM bot WHERE flows_user = $1";
-    let Workspace { id, name } = sqlx::query_as(sql)
+    let ws = sqlx::query_as(sql)
         .bind(flows_user)
-        .fetch_one(&*pool)
+        .fetch_all(&*pool)
         .await
         .map_err(|e| e.to_string())?;
 
-    let name = if let Some(n) = name {
-        format!("({n})")
-    } else {
-        String::new()
-    };
-    let display = id + &name;
-    results.push(serde_json::json!({
-        "name": display,
-    }));
+    for Workspace { id, name } in ws {
+        let name = if let Some(n) = name {
+            format!("({n})")
+        } else {
+            String::new()
+        };
+        let display = id + &name;
+        results.push(serde_json::json!({
+            "name": display,
+        }));
+    }
 
     Ok(Json(serde_json::json!({
         "title": "Connected workspace",
